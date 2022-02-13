20bf1d353a2223903e2f976b9db298934c85342b
Cristi Petre <driimus@vivaldi.net>
2022-02-13 12:34:48 +0000

    refactor: use native module for byte arr

--- packages/dynamodb-batch-iterator/src/itemIdentifier.ts
+++ packages/dynamodb-batch-iterator/src/itemIdentifier.ts
@@ -1,62 +1,64 @@
-import { AttributeMap, WriteRequest } from './types';
-import { AttributeValue } from '@aws-sdk/client-dynamodb';
-const bytes = require('utf8-bytes');
+import {Buffer} from 'node:buffer';
+import {AttributeValue} from '@aws-sdk/client-dynamodb';
+import {AttributeMap, WriteRequest} from './types';
 
 /**
  * @internal
  */
 export function itemIdentifier(
-    tableName: string,
-    { DeleteRequest, PutRequest }: WriteRequest
+	tableName: string,
+	{DeleteRequest, PutRequest}: WriteRequest,
 ): string {
-    if (DeleteRequest?.Key) {
-        return `${tableName}::delete::${serializeKeyTypeAttributes(
-            DeleteRequest.Key
-        )}`;
-    } else if (PutRequest?.Item) {
-        return `${tableName}::put::${serializeKeyTypeAttributes(
-            PutRequest.Item
-        )}`;
-    }
+	if (DeleteRequest?.Key) {
+		return `${tableName}::delete::${serializeKeyTypeAttributes(
+			DeleteRequest.Key,
+		)}`;
+	}
 
-    throw new Error(`Invalid write request provided`);
+	if (PutRequest?.Item) {
+		return `${tableName}::put::${serializeKeyTypeAttributes(
+			PutRequest.Item,
+		)}`;
+	}
+
+	throw new Error('Invalid write request provided');
 }
 
 function serializeKeyTypeAttributes(attributes: AttributeMap): string {
-    const keyTypeProperties: Array<string> = [];
-    for (const property of Object.keys(attributes).sort()) {
-        const attribute = attributes[property];
-        if (attribute.B) {
-            keyTypeProperties.push(`${property}=${toByteArray(attribute)}`);
-        } else if (attribute.N) {
-            keyTypeProperties.push(`${property}=${attribute.N}`);
-        } else if (attribute.S) {
-            keyTypeProperties.push(`${property}=${attribute.S}`);
-        }
-    }
+	const keyTypeProperties: string[] = [];
+	for (const property of Object.keys(attributes).sort()) {
+		const attribute = attributes[property];
+		if (attribute.B) {
+			keyTypeProperties.push(`${property}=${toByteArray(attribute)}`);
+		} else if (attribute.N) {
+			keyTypeProperties.push(`${property}=${attribute.N}`);
+		} else if (attribute.S) {
+			keyTypeProperties.push(`${property}=${attribute.S}`);
+		}
+	}
 
-    return keyTypeProperties.join('&');
+	return keyTypeProperties.join('&');
 }
 
-function toByteArray({ B: value }: AttributeValue.BMember): Uint8Array {
-    if (ArrayBuffer.isView(value)) {
-        return new Uint8Array(value.buffer, value.byteOffset, value.byteLength);
-    }
+function toByteArray({B: value}: AttributeValue.BMember): Uint8Array {
+	if (ArrayBuffer.isView(value)) {
+		return new Uint8Array(value.buffer, value.byteOffset, value.byteLength);
+	}
 
-    if (typeof value === 'string') {
-        return Uint8Array.from(bytes(value));
-    }
+	if (typeof value === 'string') {
+		return Uint8Array.from(Buffer.from(value).toJSON().data);
+	}
 
-    if (isArrayBuffer(value)) {
-        return new Uint8Array(value);
-    }
+	if (isArrayBuffer(value)) {
+		return new Uint8Array(value);
+	}
 
-    throw new Error('Unrecognized binary type');
+	throw new Error('Unrecognized binary type');
 }
 
 function isArrayBuffer(arg: any): arg is ArrayBuffer {
-    return (
-        (typeof ArrayBuffer === 'function' && arg instanceof ArrayBuffer) ||
-        Object.prototype.toString.call(arg) === '[object ArrayBuffer]'
-    );
+	return (
+		(typeof ArrayBuffer === 'function' && arg instanceof ArrayBuffer)
+        || Object.prototype.toString.call(arg) === '[object ArrayBuffer]'
+	);
 }
